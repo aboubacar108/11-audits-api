efbb359d9c49f9ccad2310278c8d5269dfa50b10
aboubacar108 <aboubacar.kaba108@gmail.com>
2023-02-18 09:04:05 -0500

    added User Model with custom errors

---
+++ app.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// connectDB
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication')
+
+// routers
+const authRouter = require('./routes/auth');
+const auditsRouter = require('./routes/audits');
+
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json())
+
+
+// routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/audits', authenticateUser, auditsRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+app.get('/', (req, res) => {
+     res.send('<h1>Audits API</h1><a href="/api-docs">Documentation</a>');
+});
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
