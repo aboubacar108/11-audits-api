ce6dd7e8d4d1c8ec15758401ab085ad40c0d292f
aboubacar108 <aboubacar.kaba108@gmail.com>
2023-02-21 16:20:14 -0500

    added Audit data model, CRUD operations and security features to the back-end

--- controllers/audits.js
+++ controllers/audits.js
@@ -1,21 +1,69 @@
+const Audit = require('../models/Audit')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, NotFoundError} = require('../errors')
+
+
 const getAllAudits = async (req, res) => {
-  res.send('get all audits')
+  const audits = await Audit.find({createdBy:req.user.userId}).sort('createdAt')
+  res.status(StatusCodes.OK).json({audits, count:audits.length})
 }
 
 const getAudit = async (req, res) => {
-  res.send('get single audit')
+  const {
+    user: {userId},
+    params: {id:auditId},
+  } = req
+
+  const audit = await Audit.findOne({
+    _id: auditId,
+    createdBy: userId,
+  })
+  if(!audit) {
+    throw new NotFoundError(`No audit with id:${auditId} found`)
+  }
+  res.status(StatusCodes.OK).json({audit})
 }
 
 const createAudit = async (req, res) => {
-  res.json(req.user)
+  req.body.createdBy = req.user.userId
+  const audit = await Audit.create(req.body)
+  res.status(StatusCodes.CREATED).json({audit})
 }
 
 const updateAudit = async (req, res) => {
-  res.send('update audit')
+  const {
+    body:{auditName,year,startDate,completionDate,auditFindings,timeSpent},
+    user:{userId},
+    params:{id:auditId},
+  } = req
+
+  if(auditName==='' || year==='' || startDate==='' || completionDate==='' || auditFindings==='' || timeSpent==='') {
+    throw new BadRequestError('Name, Year, Start, Completion, Findings or Time Spent fields cannot be empty')
+  }
+  const audit = await Audit.findByIdAndUpdate({_id:auditId, createdBy:userId}, req.body, {new:true,runValidators:true})
+
+  if(!audit) {
+    throw new NotFoundError(`No audit with id ${auditId} found`)
+  }
+  res.status(StatusCodes.OK).json({ audit })
 }
 
 const deleteAudit = async (req, res) => {
-  res.send('delete audit')
+  const {
+    user:{userId},
+    params:{id:auditId}
+  } = req
+
+  const audit = await Audit.findByIdAndRemove({
+    _id:auditId,
+    createdBy:userId
+  })
+
+  if(!audit) {
+    throw new NotFoundError(`No audit with id ${jobId} found`)
+  }
+
+  res.status(StatusCodes.OK).json({msg: "The entry was deleted."})
 }
 
 module.exports = {
--- models/Audit.js
+++ models/Audit.js
@@ -1,17 +1,58 @@
 const mongoose = require('mongoose')
 
-
 const AuditSchema = new mongoose.Schema({
-  name: {
+  auditName: {
     type: String, 
-    required: [true, 'must provide name'],
+    required: [true, 'Please provide audit name'],
     trim: true,
-    maxlength: [20, 'name cannot be more than 20 characters'],
+    maxlength: [200, 'Audit name cannot be more than 200 characters'],
   },
-  completed: {
-    type: Boolean,
+  period: {
+    type: String, 
+    enum: ['Q1','Q2', 'Q3','Q4'],
+    default: 'Q1',
+  },
+  year: {
+    type: String, 
+    required: [true, 'Please provide audit year'],
+    trim: true,
+    maxlength: [4, 'audit year cannot be more than 4 characters'],
+  },
+  type: {
+    type: String, 
+    enum: ['Finance','IT and Security', 'Offshore'],
+    default: 'Finance',
+  },
+  highRiskAeras: {
+    type: Boolean, 
     default: false,
   },
-})
+  startDate: {
+    type: Date, 
+  },
+  status: {
+    type: String, 
+    enum: ['Completed','In progress', 'Not started'],
+    default: 'Not started',
+  },
+  completionDate: {
+    type: Date, 
+  },
+  auditFindings: {
+    type: Number,
+    min: [1,'Completed audit must have at least one findings'],
+    max: 100,
+  },
+  timeSpent: {
+    type: Number,
+    min: [120, 'Completed audit should last at least 120 hours'],
+    max: 600,
+  },
+  createdBy:{
+    type:mongoose.Types.ObjectId,
+    ref:'User',
+    required:[true, 'Please provide user'],
+  }
+},{timestamps:true})
 
-module.exports = mongoose.model('Task', AuditSchema)
+module.exports = mongoose.model('Audit', AuditSchema)
